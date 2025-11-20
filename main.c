39a5cbd8a2c7def095cfe794166f246af4614bc6
Nix3732 <116956559+Nix3732@users.noreply.github.com>
2025-11-20 21:46:43 +0200

    Update main.c lab4

--- main.c
+++ main.c
@@ -1,33 +1,35 @@
 #include <stdint.h>
 
-void delay(uint32_t ticks) {
-    for (volatile uint32_t i = 0; i < ticks; i++);
+void wait_cycles(uint32_t cycle_count) {
+    for (volatile uint32_t i = 0; i < cycle_count; i++);
 }
 
-void SPI1_Init(void);
-void SPI1_Write(uint8_t data);
-uint8_t SPI1_Read(void);
+void initialize_spi_peripheral(void);
+void transmit_spi_byte(uint8_t byte_value);
+uint8_t receive_spi_byte(void);
 void SSD1306_Init(void);
 void SSD1306_DrawChessBoard(void);
 void SSD1306_Update(void);
 
 int main(void) {
     *(volatile uint32_t*)(0x40021000 + 0x18) |= (1 << 4);
+    
     *(volatile uint32_t*)(0x40011000 + 0x04) |= (1 << 20);
-    for(int i = 0; i < 3; i++) {
+    
+    for(int count = 0; count < 3; count++) {
         *(volatile uint32_t*)(0x40011000 + 0x0C) |= (1 << 13);
-        delay(100000);
+        wait_cycles(100000);
         *(volatile uint32_t*)(0x40011000 + 0x0C) &= ~(1 << 13);
-        delay(100000);
+        wait_cycles(100000);
     }
     
-    SPI1_Init();
+    initialize_spi_peripheral();
     SSD1306_Init();
     SSD1306_DrawChessBoard();
     SSD1306_Update();
     
     while(1) {
         *(volatile uint32_t*)(0x40011000 + 0x0C) ^= (1 << 13);
-        delay(500000);
+        wait_cycles(500000);
     }
 }
